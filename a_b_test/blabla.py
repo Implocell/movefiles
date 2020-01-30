7a961e0b9415aac89bae4cdcdde5aa6a0b9426b3
Gran <thogran@mbp15-2ggtf1.schibsted.no>
2020-01-30 08:47:13 +0100

    Removed random folder

--- a_b_test/blabla.py
+++
@@ -1,6 +0,0 @@
-dict_1 = {"mangoes":4, "apples":5, "bananas":6}
-dict_2 = {"lemons":7, "carrots":"None"}
-dict_3 = {"bread":2, "tomatoes":15}
-final_dict = [*dict_1, *dict_2, *dict_3]
-
-print(final_dict)
